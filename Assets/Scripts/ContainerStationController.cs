b4e799f0f52aafd4dc256e1ca31e0fef6098d166
glutSolidSphere <lee.w.zheng@gmail.com>
2017-08-27 23:26:35 +0800

    Containers to keep fish in. PROBABLY NEEDS STATES TOO.

---
+++ Assets/Scripts/ContainerStationController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContainerStationController : MonoBehaviour, IInteractable {
+
+    private GameObject holdSlot;
+
+    // Use this for initialization
+    void Start () {
+        holdSlot = gameObject.transform.Find("HoldSlot").gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Renderer rend = GetComponent<Renderer>();
+            rend.material.color = Color.red;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Renderer rend = GetComponent<Renderer>();
+            rend.material.color = Color.white;
+        }
+    }
+
+    public void interact () {
+    }
+
+    public void interact (GameObject otherActor) {
+        if (otherActor.tag == "Player")
+        {
+            PlayerInteractionController playerControllerScript = (PlayerInteractionController)otherActor.GetComponent(typeof(PlayerInteractionController));
+            if (playerControllerScript.getIsHolding())
+            {
+                GameObject heldObject = playerControllerScript.getHeldObject();
+                playerControllerScript.DropHeldObject();
+                FishController heldObjectControllerScript = (FishController)heldObject.GetComponent(typeof(FishController));
+                if (heldObjectControllerScript != null)
+                {
+                    heldObjectControllerScript.putIn();
+                }
+                heldObject.transform.localPosition = Vector3.zero;
+                heldObject.transform.localScale = Vector3.one;
+                heldObject.transform.SetParent(holdSlot.transform, false);
+            }
+        }
+    }
+}

3c68f963b51d43482d1936209d5c96b7e7d5f039
Daphne <won.jr@hotmail.com>
2017-08-27 17:46:16 +0800

    Created first attempt in oxygen countdown

---
+++ Assets/OxygenCountdown.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class OxygenCountdown : MonoBehaviour {
+    public float timeLeft;
+    public float rate;
+    public bool isActivated;
+    public Slider OxygenBar;
+    public Text DebugText;
+    public Image Fill;
+
+
+	// Use this for initialization
+	void Start () {
+        DebugText = GameObject.Find("Debug").GetComponent<Text>();
+        OxygenBar = GameObject.Find("OxygenBar").GetComponent<Slider>();
+        Fill = OxygenBar.GetComponentsInChildren<UnityEngine.UI.Image>().FirstOrDefault(t => t.name == "Fill");
+        timeLeft = OxygenBar.maxValue;
+        isActivated = true;
+        rate = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= rate*Time.deltaTime;
+        OxygenBar.value = timeLeft;
+        if (timeLeft < 5) {
+           if (Fill != null)
+           {
+                Fill.color = Color.red;
+           }
+        }
+
+        if (timeLeft < 0) {
+            if (Fill != null)
+            {
+                Fill.color = Color.black;
+            }
+            DebugText.text = "Game Over";
+        }
+	}
+}

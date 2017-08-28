2c74db6b97d3582db80b7766d4c982ff4a796ece
Silfer <silfer.taintedcross@gmail.com>
2017-08-28 13:00:51 +0800

    Added Script - Fish Movement (Random) - Collision with 'Obstacle' - Collision with 'Zone'

---
+++ Assets/Scripts/FishMovement.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMovement : MonoBehaviour
+{
+	public bool stop;
+	public int time = 2;
+ 	
+	public Vector3 currentPosition;
+
+	public float newPositionX;
+	public float newPositionZ;
+	public Vector3 newPosition;
+	public Vector3 collidedPosition;
+
+	public bool obstacleCrash = false;
+	public bool timeLock = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		currentPosition = transform.position;
+
+		newPositionX = currentPosition.x + Random.Range (-3.0f, 3.0f);
+		newPositionZ = currentPosition.z + Random.Range (-3.0f, 3.0f);
+		newPosition = new Vector3 (newPositionX, 1.0f, newPositionZ);
+
+		StartCoroutine(moveToPosition(newPosition, time));
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	/*void OnGUI ()
+	{
+		string clashedString = clashed.ToString ();
+		GUI.Button(new Rect(10, 10, 50, 50), clashedString);
+	}*/
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.tag == "Obstacle")
+		{
+			//Destroy(col.gameObject);
+			obstacleCrash = true;
+			//Debug.Log ("Crashed " + gameObject.name);
+		}
+
+		if (col.gameObject.tag == "Zone")
+		{
+			Destroy (gameObject);
+			Debug.Log (gameObject.name + " has entered " + col.gameObject.name); 
+		}
+	}
+
+	public IEnumerator moveToPosition(Vector3 newPosition, float time)
+	{
+		while (!stop)
+		{
+			float elapsedTime = 0;
+
+			currentPosition = transform.position;
+
+			newPositionX = currentPosition.x + Random.Range (-3.0f, 3.0f);
+			newPositionZ = currentPosition.z + Random.Range (-3.0f, 3.0f);
+			newPosition = new Vector3 (newPositionX, 1.0f, newPositionZ);
+
+			while (elapsedTime < time)
+			{
+				if (obstacleCrash == false) 
+				{
+					transform.position = Vector3.Lerp (currentPosition, newPosition, (elapsedTime / time));
+				}
+
+				else
+				{
+					if (timeLock == false) 
+					{
+						elapsedTime = 0;
+						collidedPosition = transform.position;
+						timeLock = true;
+					}
+						
+					transform.position = Vector3.Lerp (collidedPosition, currentPosition, (elapsedTime / time));
+				}
+
+				elapsedTime += Time.deltaTime;
+
+				yield return null;
+			}
+
+			timeLock = false;
+			obstacleCrash = false;
+		}
+	}
+}

552ffeed8405540a86d0259cfabf39ba05366f9e
yeojoey <yeojoey@gmail.com>
2017-09-11 15:27:09 +0800

    Fix movement and rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,20 +13,27 @@ public class PlayerController : MonoBehaviour {
 
 	void Start () {
 		rb = this.GetComponent<Rigidbody> ();
+		canMove = true;
 	}
 
 	void Update () {
 
 		if (canMove) {
 			Vector3 direction = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
-			transform.Translate (direction * movementSpeed / 100f);
+
 			if (direction != Vector3.zero) {
 				transform.rotation = Quaternion.Slerp (
 					transform.rotation,
 					Quaternion.LookRotation (direction),
 					Time.deltaTime * turnSpeed
 				);
+
+				transform.Translate (new Vector3 (0, 0, movementSpeed / 100f));		
+
 			}
+
+
+
 		}
 	}
 

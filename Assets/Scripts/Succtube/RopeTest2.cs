060b1cdec7cf9a42ef45847a13d13d6e0bce8223
Vylantze <digi9346@hotmail.com>
2017-09-16 22:39:14 +0800

    Merged Tube and Player. Used GameController as a manager.

---
+++ Assets/Scripts/Succtube/RopeTest2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeTest2 : MonoBehaviour {
+
+	//public GameObject anchor;
+	// Use this for initialization
+	void Start () {
+		//if (anchor == null)
+			//return;
+		//gameObject.AddComponent<CharacterJoint>();
+		GetComponent<ConfigurableJoint>().connectedBody=transform.parent.GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

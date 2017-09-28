4f88e2c7a8e693972be2f31e82361593471dd42e
tharain <won.jr@hotmail.com>
2017-09-28 20:59:20 +0800

    simple_move around tutorial

---
+++ Assets/Scripts/TutorialScript/tutorialController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tutorialController : MonoBehaviour {
+    public static bool isPlay = true; // this is for debugging currently
+    public static int currentTutorialStep = 0;
+    public static bool isNext = false;
+    public GameObject mainPlayer;
+
+    void Start()
+    {
+        mainPlayer = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        if(currentTutorialStep == 0)
+        {
+            if (mainPlayer.transform.position.x > 0f)
+            {
+                isNext = false;
+                tutorialGuideMovements.isTutorialGuideRunning = true;
+                next();
+            }
+        }
+    }
+
+    public static void startTutorial() {
+        isPlay = false;
+    }
+
+    public static void next()
+    {
+        if (!isNext) {
+            ++currentTutorialStep;
+        }
+        isNext = true;
+    }
+
+    public static void turnOff() {
+        isNext = false;
+    }
+}

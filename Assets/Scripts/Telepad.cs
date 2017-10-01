98b61e6e73eb573aa45b61b153a4d8613915da96
Yui Wei <tyuiwei@gmail.com>
2017-10-02 03:49:45 +0800

    telepads in the scene for both lab and sub. no animations yet soz

---
+++ Assets/Scripts/Telepad.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Telepad : MonoBehaviour
+{
+    System.Func<bool> teleportCallback;
+
+    public GameObject pairedTele;
+    private Transform teleportPoint;
+    [SerializeField] private bool allowTeleport = false;
+
+    /// <summary>
+    /// A variable that is used to set as the Player's parent, in case the location is moving around. This way, the player moves with the location. Otherwise, the Teleport Point is used
+    /// </summary>
+    [SerializeField] private Transform PlayerLocationRef;
+
+    public bool AllowTeleport
+    {
+        get
+        {
+            // Use the teleportCallback to check for conditions to teleport
+            // for when a function is required to verify if the player is allowed to teleport
+            if (teleportCallback != null)
+            {
+                return teleportCallback();
+            }
+            // Otherwise, let the developer set this setting in the Editor menu
+            else
+            {
+                return allowTeleport;
+            }
+        }
+    }
+
+    // Use this for initialization
+    public void Initialise(System.Func<bool> callback)
+    {
+        teleportPoint = pairedTele.gameObject.transform.GetChild(0);
+        teleportCallback = callback;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        PlayerController player = GameController.Obj.GetPlayerFromCollider(other);
+        if (player != null && AllowTeleport)
+        {
+            // If the player is in Character control mode
+            if (player.ControlMode == GameData.ControlType.CHARACTER &&
+                GameController.Obj.ButtonA_Up)
+            {
+                player.transform.position = teleportPoint.transform.position;
+                if (PlayerLocationRef != null)
+                {
+                    player.transform.SetParent(PlayerLocationRef);
+                }
+                else
+                {
+                    player.transform.SetParent(teleportPoint);
+                }
+            }
+        }
+    }
+}

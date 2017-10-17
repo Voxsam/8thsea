65bc3d78a39c6fd17a1b8b4ac138ef729a6c16e4
DESKTOP\Joey <yeojoey@gmail.com>
2017-10-17 17:25:31 +0800

    Set up other buttons

--- Assets/Scripts/Menus/PlayerSelectMenuController.cs
+++ Assets/Scripts/Menus/PlayerSelectMenuController.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerSelectMenuController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public Text p1;
+	public Text p2;
+	public Text p3;
+	public Text p4;
+
+	public MultiplayerManager mm;
+
+	void Update ()
+	{
+
+		if (Input.GetKey ("joystick 1 button 8")) {
+			print (8);	
+		}
+		if (Input.GetKey ("joystick 1 button 9")) {
+			print (9);	
+		}
+		if (Input.GetKey ("joystick 1 button 10")) {
+			print (10);	
+		}
+		if (Input.GetKey ("joystick 1 button 11")) {
+			print (11);	
+		}
+
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
